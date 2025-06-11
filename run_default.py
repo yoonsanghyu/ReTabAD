5bed963968b2d3363d3cb52ee7b5138ad4bb518d
yoonsanghyu <tkdgbtkdgb@naver.com>
2025-06-11 05:30:23 +0000

    remove comments in run default

--- run_default.py
+++ run_default.py
@@ -12,10 +12,6 @@ from retab.datasets import Preprocessor, DATA_LIST
 from retab.models import MODEL_LIST, TRAINERS
 from retab.utils import seed_everything, load_config, get_params, get_exp_id, args_to_dict
 
-# from torch.utils.data import DataLoader
-# from transformers import AutoTokenizer
-# from retab.datasets import TabularDataset, SerializedTabularDataset, DataCollator
-
 # load data directory configuration
 load_dotenv()
 DATA_DIR = os.getenv("DATA_DIR")
@@ -71,76 +67,12 @@ def main():
     metrics = trainer.evaluate()
     print(metrics)
     
-    # # save results
-    # result_path = os.path.join(cfg.exp.result_path, meta_info.data_name, meta_info.model_name)
-    # os.makedirs(result_path, exist_ok=True)
-    # with open(os.path.join(result_path, f"{meta_info.exp_id}.json"), 'w') as f:
-    #     json.dump(metrics, f, indent=4)
+    # save results
+    result_path = os.path.join(cfg.exp.result_path, meta_info.data_name, meta_info.model_name)
+    os.makedirs(result_path, exist_ok=True)
+    with open(os.path.join(result_path, f"{meta_info.exp_id}.json"), 'w') as f:
+        json.dump(metrics, f, indent=4)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-    # if args.serialize:
-    #     preprocessor = Preprocessor(
-    #         serialize=args.serialize,
-    #         ds_name=meta_info.data_name, 
-    #         data_dir=DATA_DIR, 
-    #         scaling_type=None, 
-    #         cat_encoding='txt_emb'
-    #     )
-    #     train_x, train_y, test_x, test_y, column_names = preprocessor.prepare_data()
-    #     train_dataset = SerializedTabularDataset.from_pandas(pd.DataFrame(train_x, columns=column_names), preserve_index=False)
-    #     tokenizer = AutoTokenizer.from_pretrained('HuggingFaceTB/SmolLM-135M')
-    #     tokenizer.pad_token = tokenizer.eos_token
-    #     train_dataset.set_tokenizer(tokenizer)
-    #     train_dataset.set_textual_columns()
-
-    #     train_dataset.prepare(is_eval=False)
-    #     data_collator = DataCollator(
-    #                         tokenizer=tokenizer,
-    #                         padding=True,
-    #                         return_tensors="pt"
-    #                     )
-    #     train_loader = DataLoader(
-    #         train_dataset,
-    #         batch_size=4,
-    #         shuffle=True,
-    #         collate_fn=data_collator,
-    #         drop_last=True,
-    #         num_workers=0,
-    #     )
-
-    #     for i, batch in enumerate(train_loader):
-    #         input_ids = batch["input_ids"]
-    #         for j in range(input_ids.size(0)):
-    #             text = tokenizer.decode(
-    #                 input_ids[j],
-    #                 skip_special_tokens=True,
-    #                 clean_up_tokenization_spaces=True
-    #             )
-    #             print(f"--- Sample {j} ---")
-    #             print(text)
-    #             print()
-    #         break
-    # else:
-    #     # load data using Preprocessor
-    #     preprocessor = Preprocessor(
-    #         serialize=args.serialize,
-    #         ds_name=meta_info.data_name, 
-    #         data_dir=DATA_DIR, 
-    #         scaling_type=data_params.scaling_type, 
-    #         cat_encoding=data_params.cat_encoding
-    #     )
-    #     train_dataset = TabularDataset(preprocessor.prepare_data()[0])
-    #     train_loader = DataLoader(
-    #         train_dataset,
-    #         batch_size=4,
-    #         shuffle=True,
-    #         drop_last=True,
-    #         num_workers=0,
-    #     )
